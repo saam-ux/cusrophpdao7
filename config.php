b4c160fa81e3826f8388a384a74601551c500ab6
Samuel Araujo <ab.samuel2707@gmail.com>
2023-03-17 09:32:26 -0300

    criamos a classe usuario

--- config.php
+++ config.php
@@ -2,11 +2,14 @@
 
 spl_autoload_register(function($class_name){
 
-    $filename = "class".DIRECTORY_SEPARATOR.$class_name. ".php";
+    $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 
     if (file_exists(($filename))) {
+
         require_once($filename);
+
     }
+
 });
 
 ?>
