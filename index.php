9861881e8e9caa156317526a5683829ab8574059
Samuel Araujo <ab.samuel2707@gmail.com>
2023-03-17 09:57:05 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
 require_once("config.php");
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(6);
+//cho $root;
 
-$root = new Usuario();
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-$root->loadById(6);
+ //Carrega um Lista de usuários buscando pelo login
+// $search = Usuario::search("jo");
+// echo json_encode($search);
 
-echo $root;
+//Carrega um Usuário usando o login e a senha
+$usuario =new Usuario();
+$usuario->login("root", "123$#");
 
+echo $usuario
 ?>
